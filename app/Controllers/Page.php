b28be0bce6bcf30af2379af1625234c5b89a660a
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-03-13 12:19:13 +0630

    added logic to controller

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -11,6 +11,14 @@ class Page extends BaseController
 
     public function view($page = 'home')
     {
-        
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page);
+
+        echo view('templates/header', $data);
+        echo view('pages/' . $page, $data);
+        echo view('templates/footer', $data);
     }
 }
