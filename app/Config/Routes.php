b539df8f765e6054314be82ca995b950fed81cac
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-03-13 12:43:12 +0630

    specified routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,7 +21,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
-$routes->setAutoRoute(true);
+$routes->setAutoRoute(false); # disabled auto routes
 
 /*
  * --------------------------------------------------------------------
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('(:any)', 'Page::view/$1');
 
 /*
  * --------------------------------------------------------------------
